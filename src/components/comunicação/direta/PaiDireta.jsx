79c36e69809da53ec0c2d4a7bf85a64253a68645
Ana Julia <anajuliadefendi@gmail.com>
2024-10-02 11:52:39 -0300

    x

--- src/components/comunicação/direta/PaiDireta.jsx
+++ src/components/comunicação/direta/PaiDireta.jsx
@@ -1,6 +1,15 @@
 export function PaiDireta(){
 
     return(
-        <div>Pai Direta</div>
+        <div className="w-full flex flex-col items-center rounded-md bg-blue-500 text-white p-7">
+            
+            <div className="flex gap-3">
+                <button className="bg-purple-500 px-4 py-2 text-xhite"> João </button>
+                <button className="bg-purple-500 px-4 py-2 text-xhite"> Maria </button>
+                <button className="bg-purple-500 px-4 py-2 text-xhite"> José </button>
+            </div>
+            
+
+        </div>
     )
 }
--- src/components/comunicação/indireta/PaiIndireta.jsx
+++ src/components/comunicação/indireta/PaiIndireta.jsx
@@ -1,6 +1,6 @@
 export function PaiIndireta(){
 
     return(
-        <div>Pai Indireta</div>
+        <div className="w-full flex flex-col items-center rounded-md bg-blue-500 text-white p-7">Pai Indireta</div>
     )
 }
