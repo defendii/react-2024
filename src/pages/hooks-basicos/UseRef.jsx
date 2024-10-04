e6fcfa199e2b375b9c6e4f78dc1556d3ec3c9893
Ana Julia <anajuliadefendi@gmail.com>
2024-10-04 09:29:46 -0300

    Update UseRef.jsx

--- src/pages/hooks-basicos/UseRef.jsx
+++ src/pages/hooks-basicos/UseRef.jsx
@@ -1,9 +1,29 @@
+import { useState } from "react";
 import { Pagina } from "../../components/Pagina";
+import { useRef } from "react";
 
 export function UseRef(){
+    const [estado, setEstado] = useState(0)
+    const referencia = useRef(0)
+
+    function incrementaEstado(){
+        setEstado(estado + 1)
+    }
+
+    function incrementaReferencia(){
+        referencia.current += 1
+    }
 
     return(
-        <Pagina titulo="useREf" subtitulo="Hooks Básicos"> 
+        <Pagina titulo="useRef" subtitulo="Hooks Básicos"> 
+            <div className="flex flex-col">
+                {`Ref: ${referencia.current} | State: ${estado}`}
+                <div>
+                    <button className="bg-red-400 p-3" onClick={incrementaReferencia}>Ref +</button>
+                    <button className="bg-purple-400 p-3" onClick={incrementaEstado}>Estado +</button>
+                </div>
+               
+            </div>
         </Pagina>
     )
 }
