7ce4eafeaf33d2760be865013604ad123f134dc0
Ana Julia <anajuliadefendi@gmail.com>
2024-10-04 08:57:59 -0300

    aa

--- src/components/comunicação/indireta/FilhoIndireta.jsx
+++ src/components/comunicação/indireta/FilhoIndireta.jsx
@@ -5,6 +5,28 @@ export function FilhoIndireta(){
             w-full flex flex-col items-center p-7 rounded-md
             bg-red-500 text-white 
         `}>
-        Filho Indireta</div>
+         <div className="flex gap-3">
+            <button 
+                className="botao"
+                onClick={() => setNome("João")}
+            >
+                João
+            </button>
+            <button 
+                className="botao" 
+                onClick={() => setNome("Maria")}
+            >
+                Maria
+            </button>
+            
+            <button 
+                className="botao" 
+                onClick={() => setNome("José")}
+            >
+                José
+            </button>
+        
+        </div>
+    </div>
     )
 } 
--- src/components/comunicação/indireta/PaiIndireta.jsx
+++ src/components/comunicação/indireta/PaiIndireta.jsx
@@ -3,41 +3,23 @@ import { FilhoIndireta } from "./FilhoIndireta"
 
 export function PaiIndireta(){
 
-        const [nome, setNome] = useState("")
+    const [nome, setNome] = useState("")
+
+    function alterarNome(novoNome){
+        setNome(novoNome)
+    }
 
     return (
         <div
-      className={`
+        className={`
             w-full flex flex-col items-center p-7 rounded-md
             bg-blue-500 text-white gap-4
         `}
-    >
-      <div className="flex gap-3">
-        <button 
-            className="botao"
-            onClick={() => setNome("João")}
         >
-            João
-        </button>
-        <button 
-            className="botao" 
-            onClick={() => setNome("Maria")}
-        >
-            Maria
-        </button>
-        
-        <button 
-            className="botao" 
-            onClick={() => setNome("José")}
-        >
-            José
-        </button>
-        
-      </div>
-
+     
       <span className="text-4xl p-5">{nome}</span>
      
-        <FilhoIndireta></FilhoIndireta>
+        <FilhoIndireta alterar={alterarNome}></FilhoIndireta>
         
 
     </div>
