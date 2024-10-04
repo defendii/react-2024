f781c6fe9c2a9bde8bb11555d857c1d3e9721a78
Ana Julia <anajuliadefendi@gmail.com>
2024-10-04 09:46:55 -0300

    dog

--- src/pages/hooks-basicos/UseEffect.jsx
+++ src/pages/hooks-basicos/UseEffect.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 import { Pagina } from "../../components/Pagina";
 
 export function UseEffect(){
+    const [url, setUrl] = useState("")
+
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(resposta => resposta.json())
+        .then(objeto => {
+            console.log(objeto)
+            setUrl(objeto.message)
+        })
+    }, [])
 
     return(
         <Pagina titulo="useEffect" subtitulo="Hooks Básicos"> 
+            <img src={url} alt="" />
         </Pagina>
     )
 }
